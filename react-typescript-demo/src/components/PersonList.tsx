121bd209313575282bb500142035a4ae39a6a474
Venkatesh goud <goudvenki5@gmail.com>
2026-02-05 08:53:38 -0800

    typescript props commit

--- react-typescript-demo/src/components/PersonList.tsx
+++ react-typescript-demo/src/components/PersonList.tsx
@@ -10,6 +10,8 @@ export const PersonList=(props:PersonListProps)=>{
         {props.names.map((name,index)=>(
             <h2 key={index}>{name.first} {name.last}</h2>
         ))}
+        first: {props.names[0].first}
+        second: {props.names[1].first}
         </div>
     )
 }
